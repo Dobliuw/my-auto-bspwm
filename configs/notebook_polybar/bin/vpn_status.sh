28d0b3e205c46f3fa7aff6f11871b9a96193e2f3
dobliuw <dobliuw@dobliuw.com>
2026-02-05 17:58:56 +0000

    Modifying vpn script for recognice NordVPN

--- configs/notebook_polybar/bin/vpn_status.sh
+++ configs/notebook_polybar/bin/vpn_status.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-ip=$(/usr/sbin/ifconfig | /usr/bin/grep -C 1 -E  "tun0|nordlynx" | /usr/bin/tail -n 1 | /usr/bin/awk '{print $2}')
+while true; do
 
-[[ $ip ]] && echo "%{F#09d304}󱛇 $ip%{F#ffffff}" || echo "%{F#d93753}󱛇 Disconnected.%{F#ffffff}"
+    nordvpn=$(/usr/sbin/ifconfig | /usr/bin/grep -i "nordlynx")
+
+    if [ "$nordvpn" ]; then
+        if [ ! "$ip" ]; then
+            ip=$(curl -s https://ipinfo.io | grep -i "\"ip\"" | awk '{print $2}' | tr -d "\"" | tr -d ",")
+        fi
+    else
+        ip=$(/usr/sbin/ifconfig | /usr/bin/grep -C 1 -E "tun0|nordlynx" | /usr/bin/tail -n 1 | /usr/bin/awk '{print $2}')
+    fi
+    echo $ip $nordvpn
+    [[ $ip ]] && echo "%{F#09d304}󱛇 $ip%{F#ffffff}" || echo "%{F#d93753}󱛇 Disconnected.%{F#ffffff}"
+
+done
