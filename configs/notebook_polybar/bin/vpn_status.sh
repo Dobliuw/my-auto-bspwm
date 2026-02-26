2f51f252c4f95bef0eeef30d06cfa9895511063f
dobliuw <dobliuw@dobliuw.com>
2026-02-26 18:26:39 +0000

    Modifying vpn detection script to handle nordvpn connections

--- configs/notebook_polybar/bin/vpn_status.sh
+++ configs/notebook_polybar/bin/vpn_status.sh
@@ -1,17 +1,53 @@
 #!/bin/bash
 
+last_state=""
+ip=1
+
 while true; do
 
-    nordvpn=$(/usr/sbin/ifconfig | /usr/bin/grep -i "nordlynx")
+    # En vez de guardar texto, guardamos el exit status (0 o 1)
+    /usr/sbin/ifconfig | /usr/bin/grep -qi "nordlynx"
+    nordvpn=$?
 
-    if [ "$nordvpn" ]; then
-        if [ ! "$ip" ]; then
-            ip=$(curl -s https://ipinfo.io | grep -i "\"ip\"" | awk '{print $2}' | tr -d "\"" | tr -d ",")
-        fi
+    /usr/sbin/ifconfig | /usr/bin/grep -qi "tun0"
+    other_vpn=$?
+
+    if [ "$nordvpn" -eq 0 ] || [ "$other_vpn" -eq 0 ]; then
+        current_state="connected"
     else
-        ip=$(/usr/sbin/ifconfig | /usr/bin/grep -C 1 -E "tun0|nordlynx" | /usr/bin/tail -n 1 | /usr/bin/awk '{print $2}')
+        current_state="disconnected"
     fi
-    echo $ip $nordvpn
-    [[ $ip ]] && echo "%{F#09d304}󱛇 $ip%{F#ffffff}" || echo "%{F#d93753}󱛇 Disconnected.%{F#ffffff}"
 
+    if [ "$current_state" != "$last_state" ]; then
+
+        if [ "$nordvpn" -eq 0 ]; then
+
+            if [ "$current_state" = "connected" ]; then
+                ip=$(curl -s https://api.ipify.org)
+            else
+                ip=1
+            fi
+
+        elif [ "$other_vpn" -eq 0 ]; then
+
+            if [ "$current_state" = "connected" ]; then
+                ip=$(/usr/sbin/ifconfig | /usr/bin/grep -A1 "tun0" | /usr/bin/tail -n1 | /usr/bin/awk '{print $2}')
+            else
+                ip=1
+            fi
+
+        else
+            ip=1
+        fi
+
+        last_state="$current_state"
+    fi
+
+    if [ "$ip" != "1" ]; then
+        echo "%{F#09d304}󱛇 $ip%{F#ffffff}"
+    else
+        echo "%{F#d93753}󱛇 Disconnected.%{F#ffffff}"
+    fi
+
+    sleep 10
 done
